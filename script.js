6e6acbd849a94cc3585a190d8b9f24d888b24a4e
tony8464 <46433974+tony8464@users.noreply.github.com>
2021-07-20 00:00:57 -0500

    added features to detect when NaN is entered, and style/js clean up

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const addButton = document.getElementById('plus-one');
 const minusButton = document.getElementById('minus-one');
-
 const counterEl = document.getElementById('counter');
-let counterValue = parseFloat(counterEl.value)
 
 
-//just another way of doing the same thing:
+
+function checkIfCounterValueIsNaN(num){
+    if (isNaN(num)){
+        document.getElementById('container').classList.add('error')
+        document.getElementById('NaN-error').style.visibility = ('visible')
+    } else {
+        document.getElementById('container').classList.remove('error')
+        document.getElementById('NaN-error').style.visibility = ('hidden')
+    }
+}
+
 function plusCounter() {
+    let counterValue = parseFloat(counterEl.value);
+    checkIfCounterValueIsNaN(counterValue);
     counterEl.value = counterValue +=1;
 }
 function minusCounter() {
+    let counterValue = parseFloat(counterEl.value);
+    checkIfCounterValueIsNaN(counterValue);
     counterEl.value = counterValue -=1;
 }
-
 
 //event listeners
 addButton.addEventListener('click', plusCounter)
